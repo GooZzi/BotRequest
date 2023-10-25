8aacc8df5cfad87c69be6b53d2f3033773aa295b
GooZzi <140390407+GooZzi@users.noreply.github.com>
2023-10-25 23:34:29 +0300

    Update Message.php

--- Message.php
+++ Message.php
@@ -1,29 +1,26 @@
 <?php
 
 // Получите токен бота от BotFather в Telegram
-$token = 'YOUR_BOT_TOKEN';
+$token = '6365069296:AAFIUYKuOr1OPg3datUM6prHNUD6u03V2sg';
 
-// Установите вебхук для вашего бота
+// Получите обновления от Telegram API
 $base_url = 'https://api.telegram.org/bot' . $token . '/';
-$webhook_url = 'https://your-domain.com/your-webhook-url.php';
-file_get_contents($base_url . 'setWebhook?url=' . $webhook_url);
+$update_url = $base_url . 'getUpdates';
 
-// Обработка входящих запросов
-$update = json_decode(file_get_contents('php://input'), true);
+// Получаем последнее обновление
+$response = file_get_contents($update_url);
+$data = json_decode($response, true);
+$last_update = end($data['result']);
+$message = $last_update['message'];
 
-// Проверяем, есть ли новое входящее сообщение
-if (isset($update['message'])) {
-    $message = $update['message'];
-    
-    // Получаем ID чата, откуда пришло сообщение
-    $chat_id = $message['chat']['id'];
-    
-    // Получаем текст сообщения
-    $text = $message['text'];
-    
-    // Отправляем сообщение в группу
-    $group_chat_id = 'YOUR_GROUP_CHAT_ID';
-    file_get_contents($base_url . 'sendMessage?chat_id=' . $group_chat_id . '&text=' . urlencode($text));
-}
+// Получаем ID чата, откуда пришло сообщение
+$chat_id = $message['chat']['id'];
+
+// Получаем текст сообщения
+$text = $message['text'];
+
+// Отправляем сообщение в группу
+$group_chat_id = '-1001980493060';
+file_get_contents($base_url . 'sendMessage?chat_id=' . $group_chat_id . '&text=' . urlencode($text));
 
 ?>
